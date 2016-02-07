6a6fcb5c0a1bdc812433e7f5ec3f5bd488237feb
Simon <simin_jie@163.com>
2016-02-07 23:29:01 +0800

    在 views.py 中渲染模板

--- rango/views.py
+++ rango/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):#request is HttpRequest object 
-    return HttpResponse('Rango says hey there world!<br/><a href="/rango/about">about</a>')
+    context_dict = {'boldmessage': "I am bold font from the context."}
+    
+    return render(request, 'rango/index.html', context_dict)
     
 def about(request):
     return HttpResponse('Here is the about page.<br/> <a href="/rango/">main page</a>')
