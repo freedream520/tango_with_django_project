1056179bdbca07740f6f51045322da6e4a4a1198
Simon <simin_jie@163.com>
2016-02-07 21:40:45 +0800

    分别在 index and about 页面中添加对面的超链接

--- rango/views.py
+++ rango/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):#request is HttpRequest object 
-    return HttpResponse('Rango says hey there world!')
+    return HttpResponse('Rango says hey there world!<br/><a href="/rango/about">about</a>')
+    
+def about(request):
+    return HttpResponse('Here is the about page.<br/> <a href="/rango/">main page</a>')
