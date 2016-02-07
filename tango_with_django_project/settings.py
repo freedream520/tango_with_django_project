6ea62d650bfccf7ad78ee2d95bc13a6d69db419e
Simon <simin_jie@163.com>
2016-02-07 23:25:52 +0800

    设置中添加 TEMPLATE_DIRS 路径

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -53,6 +53,12 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 
 WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
+TEMPLATE_DIRS = [
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
