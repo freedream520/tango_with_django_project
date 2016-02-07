28e250e148ce5f5a6f3c99f97140f10f4e0f5c2a
Simon <simin_jie@163.com>
2016-02-07 14:16:43 +0800

    创建是视图函数

--- rango/views.py
+++ rango/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def index(request):#request is HttpRequest object 
+    return HttpResponse('Rango says hey there world!')
