4254167cc321c3acc2e145c0791b27d86b47cd3d
Simon <simin_jie@163.com>
2016-02-08 00:02:09 +0800

    在设置中添加 STATICFILES_DIRS

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -88,3 +89,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#不是 STATIC_DIRS 虽然 TEMPLATE_DIRS
+STATICFILES_DIRS = (
+    STATIC_PATH,
+)
