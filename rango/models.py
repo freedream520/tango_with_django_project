cdb2bfaa3d96c99895ccf371166a3a589af1fa37
Simon <simin_jie@163.com>
2016-02-10 16:48:07 +0800

    主页显示分类———templates object.name

--- rango/models.py
+++ rango/models.py
@@ -11,7 +11,7 @@ class Category(models.Model):
         return self.name 
         
 class Page(models.Model):
-    category = models.ForeignKey(Category)#不是Category.name？
+    category = models.ForeignKey(Category)#表示的是 Category object 
     title = models.CharField(max_length=128)
     url = models.URLField()
     views = models.IntegerField(default=0)
