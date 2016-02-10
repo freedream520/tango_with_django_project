c4d54406a531131f9eef1404a72116877ea9efc6
Simon <simin_jie@163.com>
2016-02-10 19:03:54 +0800

    category 视图函数

--- rango/views.py
+++ rango/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from rango.models import Category
+from rango.models import Category, Page 
 
 def index(request):#request is HttpRequest object 
     #从 model 中取出 top 5，传递到 templates 中 
@@ -9,5 +9,19 @@ def index(request):#request is HttpRequest object
     
     return render(request, 'rango/index.html', context_dict)
     
-def about(request):
-    return render(request, 'rango/about.html')
+def category(request, category_name_slug):
+    context_dict = {}
+    
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+        context_dict['category'] = category
+        context_dict['category_name'] = category.name        
+        
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages 
+    except Category.DoesNotExist:
+        #不做任何事， templates 显示 'no category'
+        pass 
+        
+        
+        
