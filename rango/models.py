107fc03c3b56f5baece1ad6c1cb9faca575ab63f
Simon <simin_jie@163.com>
2016-02-10 10:31:21 +0800

    创建数据库模型

--- rango/models.py
+++ rango/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#从 django.db.models.Model 继承
+#不同类型的 Field and validataors
+class Category(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    #Like __str__ 当 print 时打印出的字符串
+    def __unicode__(self):
+        return self.name 
+        
+class Page(models.Model):
+    category = models.ForeinKey(Category)#不是Category.name？
+    title = models.CharField(max_length=128)
+    url = models.URLField()
+    views = models.IntegerField(default=0)
+    
+    def __unicode__(self):
+        return self.title 
+
