157c467270399018e139232b9fe177fc45b8c786
Simon <simin_jie@163.com>
2016-02-10 19:43:43 +0800

    index.html 上添加 top 5 viewed pages

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,9 @@ def index(request):#request is HttpRequest object
     category_list = Category.objects.order_by('-likes')[:5] # '-' 降序
     context_dict = {'categories': category_list}
     
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
+    
     return render(request, 'rango/index.html', context_dict)
     
 def category(request, category_name_slug):
