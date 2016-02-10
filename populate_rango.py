7b0c9da96b50f1a45678674bf93b6c842e0b6016
Simon <simin_jie@163.com>
2016-02-10 14:16:55 +0800

    更新 add_category 函数，增加 views and likes

--- populate_rango.py
+++ populate_rango.py
@@ -8,7 +8,7 @@ django.setup()
 from rango.models import Category, Page 
 
 def populate():
-    python_category = add_category('Python')
+    python_category = add_category('Python', views=128, likes=64)
     
     add_page(category=python_category,
         title="Official Python Tutorial",
@@ -22,7 +22,7 @@ def populate():
         title="Learn Python in 10 Minutes",
         url="http://www.korokithakis.net/tutorials/python/")
         
-    django_category = add_category('Django')
+    django_category = add_category('Django', views=64, likes=32)
     
     add_page(category=django_category,
         title="Official Django Tutorial",
@@ -36,7 +36,7 @@ def populate():
         title="How to Tango with Django",
         url="http://www.tangowithdjango.com/")
         
-    frame_category = add_category("Other Frameworks")
+    frame_category = add_category("Other Frameworks", views=32, likes=16)
     
     add_page(category=frame_category,
         title="Bottle",
@@ -48,16 +48,16 @@ def populate():
         
     #打印出添加的东西
     for c in Category.objects.all():
-        for p in Page.objects.all(category=c):
-            print("- {0} - {1}".format(str(c), str(p)))
+        for p in Page.objects.filter(category=c):
+            print("- {0} - {1}".format(str(c), str(p.title)))
     
 def add_page(category, title, url, views=0):
     p = Page.objects.get_or_create(category=category, title=title, 
-                                    url=url, views=views)[0] #tuple 中的第一个
+                                    url=url, views=views)[0] #tuple 中的第一个    
     return p 
     
-def add_category(name):
-    c = Category.objects.get_or_create(name=name)[0]
+def add_category(name, views=0, likes=0):
+    c = Category.objects.get_or_create(name=name, views=views, likes=likes)[0]
     return c 
     
 if __name__ == '__main__':
