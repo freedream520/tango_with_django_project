81e1f517e032f896c681d14c7b0a47b2a963b811
Simon <simin_jie@163.com>
2016-02-11 15:26:40 +0800

    修改 add_page() 中渲染的模板名称

--- rango/views.py
+++ rango/views.py
@@ -75,7 +75,7 @@ def add_page(request, category_name_slug):
             
         context_dict = {'form': form, 'category': category}
         
-    return render(request, 'rango/add_category.html', context_dict)
+    return render(request, 'rango/add_page.html', context_dict)
     
         
         
