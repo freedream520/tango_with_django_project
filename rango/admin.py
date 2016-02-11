b07d4f3d32cbbbfba2b1f012232b384ae2496c2b
Simon <simin_jie@163.com>
2016-02-11 20:57:20 +0800

    在 admin.py 中注册，使管理界面包含新增模型

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from rango.models import Category, Page 
+from rango.models import Category, Page, UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -12,4 +12,5 @@ class CategoryAdmin(admin.ModelAdmin):
 # Register models
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
 
