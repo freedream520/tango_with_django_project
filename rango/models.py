5cd4902888be41e49eecb3d64a2cc3a2ed6be836
Simon <simin_jie@163.com>
2016-02-11 20:55:18 +0800

    增加 UserProfile model 来添加 User 中的 attributes

--- rango/models.py
+++ rango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.template.defaultfilters import slugify
+from django.contrib.auth.models import User 
 
 #从 django.db.models.Model 继承
 #不同类型的 Field and validataors
@@ -27,4 +28,17 @@ class Page(models.Model):
     
     def __unicode__(self):
         return self.title 
+        
+class UserProfile(models.Model):
+    #将当前模型与 User 模型连接
+    #因为其他应用可能也要使用 User，所以使用这个而不是继承自 User 
+    user = models.OneToOneField(User)
+    
+    #自定义的 attributes      #允许不填写
+    website = models.URLField(blank=True)
+                                #与设置中的 MEDIA_ROOT join 组成储存图片文件夹
+    picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def __str__(self):
+        return self.user.username 
 
