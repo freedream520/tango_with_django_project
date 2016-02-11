b984f884a815a698b9dc523284db53ea3070d764
Simon <simin_jie@163.com>
2016-02-11 14:29:34 +0800

    add_category url 与 视图函数映射

--- rango/urls.py
+++ rango/urls.py
@@ -2,12 +2,11 @@ from django.conf.urls import patterns, url
 from rango import views 
 
 urlpatterns = patterns('',
-        url(r'^$', views.index, name='index'),        
+        url(r'^$', views.index, name='index'),     
+        url(r'^add_category', views.add_category, name='add_category'),
         #子串匹配到的内容将可以用命名的name参数来提取
         url(r'^category/(?P<category_name_slug>[\w\-]+)/$', 
             views.category, name='category'),
         )
-        #\w a-z, A-Z, 0-9, including the _
-        #\- 指 '-' 匹配 a-z, A-Z, 0-9 _ and - 
-        #<category_name_slug> 与 views.py 中的参数名一样
+        
         
