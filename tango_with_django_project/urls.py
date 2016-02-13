fec6c5703168d6c2d4cbd5dd4b1a2f3c350ebfef
Simon <simin_jie@163.com>
2016-02-13 19:15:21 +0800

    创建 registration/login.html

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     #匹配到  ^rango 就使用 rango.urls 中的匹配
     url(r'^rango/', include('rango.urls')),
-    url(r'accounts/', include('registration.backends.simple.urls')),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
 )
 
 #对所有 以 media/ 开头的请求将会传递给 'django.views.static'——视图函数 static
