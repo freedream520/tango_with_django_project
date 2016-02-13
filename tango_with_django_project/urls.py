d64dd5c9b8a88e9556b61d74d3c1ded6abb480a9
Simon <simin_jie@163.com>
2016-02-13 21:16:27 +0800

    添加修改密码模板

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -1,16 +1,21 @@
 from django.conf import settings #相当于 project 中的 settings.py 
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from registration.backends.simple.views import RegistrationView
+
+#注册成功后转向 index 页面
+class MyRegistrationView(RegistrationView):
+    #The URL to redirect to after successful registration
+    def get_success_url(self, request, user):        
+        return '/rango/'
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tango_with_django_project.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
     #匹配到  ^rango 就使用 rango.urls 中的匹配
     url(r'^rango/', include('rango.urls')),
-    url(r'^accounts/', include('registration.backends.simple.urls')),
+                                #as_view() 给 Class 提供一个函数入口
+    url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/', include('registration.backends.simple.urls')),    
 )
 
 #对所有 以 media/ 开头的请求将会传递给 'django.views.static'——视图函数 static
