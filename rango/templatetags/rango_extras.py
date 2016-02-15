22766543ab2c43cfcb55105d8eaf4c1542a15b08
Simon <simin_jie@163.com>
2016-02-15 13:23:18 +0800

    添加templatetag变量 act_cat

--- rango/templatetags/rango_extras.py
+++ rango/templatetags/rango_extras.py
@@ -5,4 +5,6 @@ register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
 def get_category_list():
-    return {"cats": Category.objects.all()}
+    # key 表示传递到 cats.html 的变量名 
+    # 通过应用模板中的 {% get_category_list category %} 传递
+    return {"cats": Category.objects.all(), 'act_cat': cat}
