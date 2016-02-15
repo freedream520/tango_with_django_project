91ff54df1b680fa7e1102e939f85c9de7e45d173
Simon <simin_jie@163.com>
2016-02-15 13:26:58 +0800

    添加默认参数 cat

--- rango/templatetags/rango_extras.py
+++ rango/templatetags/rango_extras.py
@@ -4,7 +4,7 @@ from rango.models import Category
 register = template.Library()
 
 @register.inclusion_tag('rango/cats.html')
-def get_category_list():
+def get_category_list(cat=None):
     # key 表示传递到 cats.html 的变量名 
     # 通过应用模板中的 {% get_category_list category %} 传递
     return {"cats": Category.objects.all(), 'act_cat': cat}
