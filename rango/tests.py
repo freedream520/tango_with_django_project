3b9ec0bcce97f0578b419755be1c02c52fdd63cf
Simon <simin_jie@163.com>
2016-02-17 20:48:07 +0800

    测试 model 中的 views 是否 >= 0

--- rango/tests.py
+++ rango/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from rango.models import Category
 
-# Create your tests here.
+class CategoryMethodTests(TestCase):
+    def test_ensure_views_are_positive(self):
+        """
+        views 应该 >= 0 
+        """
+        cat = Category(name='test', views=-1, likes=0)
+        cat.save()
+        self.assertEqual((cat.views >= 0), True)
